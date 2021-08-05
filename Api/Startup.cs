709b13cf676c351625bfd5bc86ebd80b2548ba96
Pavlo Voloshyn <pavlo-voloshyn@users.noreply.github.com>
2021-08-05 21:22:40 +0300

    Configure swagger for auth

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -32,10 +32,7 @@ namespace Api
                 .AddDefaultTokenProviders();
             services.ConfigureMapper();
             services.ConfigureServices();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
-            });
+            services.ConfigureSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
